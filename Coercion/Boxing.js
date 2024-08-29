b1d6d81d713c75b8c06f5137df294767dcb567ef
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2024-08-29 14:38:12 +0530

    Boxing

---
+++ Coercion/Boxing.js
@@ -0,0 +1,6 @@
+//console.log(1.toString());//Error
+console.log("abc".toString());//"abc"
+//console.log(undefined.toString());//Error
+//console.log((undefined).toString());//Error
+console.log((1).toString());//1
+console.log(Number(1).toString());//1
